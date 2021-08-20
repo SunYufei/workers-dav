5f4b618089ccf1d232303e4fb58ea8cb3b76ceb7
SunYufei <sunyufei963@qq.com>
2021-08-20 17:26:10 +0800

    test: jest test framework

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+    moduleFileExtensions: [
+        "ts",
+        "js"
+    ],
+    modulePathIgnorePatterns: [
+        "<rootDir>/dist/",
+        "<rootDir>/worker/"
+    ],
+    modulePaths: [
+        "<rootDir>/src/"
+    ],
+    preset: "ts-jest",
+    testMatch: [
+        "**/test/**/*.[jt]s?(x)",
+        "**/test/**/?(*.)+(spec|test).[jt]s?(x)"
+    ],
+    testPathIgnorePatterns: [
+        "/node_modules/",
+        "dist",
+        "worker"
+    ]
+}
+
+export default config;
